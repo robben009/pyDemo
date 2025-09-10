70c1143094997aa4514ad24e5272c5cb496759e2
放山 <hujingzheng.hjz@alibaba-inc.com>
2025-09-10 14:18:47 +0800

    首次提交2

--- base/functionDemo.py
+++ base/functionDemo.py
@@ -1,3 +1,8 @@
+# 函数是第一类对象（First-Class Objects）。这意味着函数可以像其他数据类型一样被：
+# 赋值给变量
+# 作为参数传递给其他函数
+# 作为函数的返回值
+# 存储在数据结构中
 # 函数可以赋值给变量
 def say_hello():
     print("Hello!")
@@ -23,6 +28,15 @@ greeting_func()  # 输出: Hello!
 numbers = [1, 2, 3, 4, 5]
 squared = map(lambda x: x**2, numbers)
 print(list(squared))  # 输出: [1, 4, 9, 16, 25]
+
+
+def outer_function(x):
+    def inner_function(y):
+        return x + y  # 内部函数可以访问外部函数的变量x
+    return inner_function
+
+closure = outer_function(10)
+print(closure(5))  # 输出: 15
 
 
 
